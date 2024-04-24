9014fd214c23d092f2196cee51890f631ca2d8f1
NebelNidas <nebelnidas@gmail.com>
2024-04-24 12:18:36 +0200

    Add Recaf Simple reader visitEnd test

--- src/test/java/net/fabricmc/mappingio/visiting/VisitEndTest.java
+++ src/test/java/net/fabricmc/mappingio/visiting/VisitEndTest.java
@@ -108,6 +108,12 @@ public class VisitEndTest {
 	}
 
 	@Test
+	public void recafSimpleFile() throws Exception {
+		MappingFormat format = MappingFormat.RECAF_SIMPLE_FILE;
+		check(format);
+	}
+
+	@Test
 	public void jobfFile() throws Exception {
 		MappingFormat format = MappingFormat.JOBF_FILE;
 		check(format);
